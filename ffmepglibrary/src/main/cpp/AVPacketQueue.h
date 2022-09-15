f73238ab1b81ccec75deb60a6f7438cbb95ef9c5
yangtianzheng <yangtianzheng1@gmail.com>
2022-09-15 21:40:26 +0800

    添加音视频解码数据管理队列

--- ffmepglibrary/src/main/cpp/AVPacketQueue.h
+++ ffmepglibrary/src/main/cpp/AVPacketQueue.h
@@ -5,9 +5,48 @@
 #ifndef VIDEOPLAYER_AVPACKETQUEUE_H
 #define VIDEOPLAYER_AVPACKETQUEUE_H
 
+#include <thread>
+
+extern "C"{
+#include <libavcodec/packet.h>
+};
+
+using namespace std;
+
+typedef struct AVPacketNode{
+    AVPacket pkt;
+    struct AVPacketNode *next;
+} AVPacketNode;
 
 class AVPacketQueue {
+public:
+    AVPacketQueue();
+    virtual ~AVPacketQueue();
+    int PushPacket(AVPacket *pkt);
+    int PushNullPacket(int stream_index);
+    void Flush();
+    void Abort();
+    void Start();
 
+    int GetPacket(AVPacket *pkt);
+    int GetPacket(AVPacket *pkt, int block);
+    int GetPacketSize();
+    int GetSize();
+    int64_t GetDuration();
+    int IsAbort();
+
+private:
+    int Put(AVPacket *pkt);
+
+private:
+    mutex m_Mutex;
+    condition_variable m_CondVar;
+    AVPacketNode *first_pkt;
+    AVPacketNode *last_pkt;
+    int nb_packets;
+    int size;
+    int64_t duration;
+    volatile int abort_request;
 };
 
 
